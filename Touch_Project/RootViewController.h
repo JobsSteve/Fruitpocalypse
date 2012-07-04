30ba398f4309223ef8897c844743d070bb53311a
Артур <Artur@MacBook-Pro.local>
2012-07-04 10:22:30 +0400

    Initial Commit

---
+++ Touch_Project/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  Touch_Project
+//
+//  Created by Артур on 04.07.12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
